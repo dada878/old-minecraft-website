92bb791d42d60d518d02b3436de0a0c12b4f0397
dada909090 <37009584+dada909090@users.noreply.github.com>
2021-07-30 20:10:22 +0800

    Add files via upload

--- json_command_maker.js
+++ json_command_maker.js
@@ -1,3 +1,8 @@
+//刪除的文字返回輸入區
+//顏色表示
+//預覽字體減小
+//html增長
+
 var command_text = [];
 var cmd_text = [];
 
@@ -44,7 +49,7 @@ function show(output_text) {
         command_text.push(output_text);
     }
     if (output_text != "dedall") {
-        command2.innerHTML = ((cmd_text.toString())).replaceAll(",","").replaceAll("\\n","<br>");
+        command2.innerHTML = textchage((cmd_text.toString())).replaceAll(",","").replaceAll("\\n","<br>");
         if (list_valu == "set1") {
             command.innerHTML = 'execute @a ~~~ titleraw @s actionbar {"rawtext":['+command_text+']}';
         }
@@ -113,7 +118,7 @@ function copyText(id) {
         range.moveToElementText(node);
         range.select();
         document.execCommand("copy");
-        alert("複製成功!");
+        alert("指令複製成功!");
     } else if (window.getSelection) {
         var selection = window.getSelection();
         var range = document.createRange();
@@ -126,4 +131,63 @@ function copyText(id) {
         alert('無法複製內容、瀏覽器不支援');       
     }
     window.getSelection().removeAllRanges();
+}
+
+function textchage(element_text) {
+    text = element_text;
+    textlist = (text.split("§"));
+    for (i=1;i<textlist.length;i++) {
+        if (textlist[i].substring(0,1)=="1") {
+            textlist[i]='<p style="color:#0000AA;display:inline;display:inline;">'+ (textlist[i].toString()).substring(1,(textlist[i].toString()).length) +'</p>';
+        }
+        if (textlist[i].substring(0,1)=="0") {
+            textlist[i]='<p style="color:#000000;display:inline;">'+ (textlist[i].toString()).substring(1,(textlist[i].toString()).length) +'</p>';
+        }
+        if (textlist[i].substring(0,1)=="2") {
+            textlist[i]='<p style="color:#00AA00;display:inline;display:inline;">'+ (textlist[i].toString()).substring(1,(textlist[i].toString()).length) +'</p>';
+        }
+        if (textlist[i].substring(0,1)=="3") {
+            textlist[i]='<p style="color:#00AAAA;display:inline;">'+ (textlist[i].toString()).substring(1,(textlist[i].toString()).length) +'</p>';
+        }
+        if (textlist[i].substring(0,1)=="4") {
+            textlist[i]='<p style="color:#AA0000;display:inline;">'+ (textlist[i].toString()).substring(1,(textlist[i].toString()).length) +'</p>';
+        }
+        if (textlist[i].substring(0,1)=="5") {
+            textlist[i]='<p style="color:#AA00AA;display:inline;">'+ (textlist[i].toString()).substring(1,(textlist[i].toString()).length) +'</p>';
+        }
+        if (textlist[i].substring(0,1)=="6") {
+            textlist[i]='<p style="color:#FFAA00;display:inline;">'+ (textlist[i].toString()).substring(1,(textlist[i].toString()).length) +'</p>';
+        }
+        if (textlist[i].substring(0,1)=="7") {
+            textlist[i]='<p style="color:#AAAAAA;display:inline;">'+ (textlist[i].toString()).substring(1,(textlist[i].toString()).length) +'</p>';
+        }
+        if (textlist[i].substring(0,1)=="8") {
+            textlist[i]='<p style="color:#555555;display:inline;">'+ (textlist[i].toString()).substring(1,(textlist[i].toString()).length) +'</p>';
+        }
+        if (textlist[i].substring(0,1)=="9") {
+            textlist[i]='<p style="color:#5555FF;display:inline;">'+ (textlist[i].toString()).substring(1,(textlist[i].toString()).length) +'</p>';
+        }
+        if (textlist[i].substring(0,1)=="a") {
+            textlist[i]='<p style="color:#55FF55;display:inline;">'+ (textlist[i].toString()).substring(1,(textlist[i].toString()).length) +'</p>';
+        }
+        if (textlist[i].substring(0,1)=="b") {
+            textlist[i]='<p style="color:#55FFFF;display:inline;">'+ (textlist[i].toString()).substring(1,(textlist[i].toString()).length) +'</p>';
+        }
+        if (textlist[i].substring(0,1)=="c") {
+            textlist[i]='<p style="color:#FF5555;display:inline;">'+ (textlist[i].toString()).substring(1,(textlist[i].toString()).length) +'</p>';
+        }
+        if (textlist[i].substring(0,1)=="d") {
+            textlist[i]='<p style="color:#FF55FF;display:inline;">'+ (textlist[i].toString()).substring(1,(textlist[i].toString()).length) +'</p>';
+        }
+        if (textlist[i].substring(0,1)=="e") {
+            textlist[i]='<p style="color:#FFFF55;display:inline;">'+ (textlist[i].toString()).substring(1,(textlist[i].toString()).length) +'</p>';
+        }
+        if (textlist[i].substring(0,1)=="f") {
+            textlist[i]='<p style="color:#FFFFFF;display:inline;">'+ (textlist[i].toString()).substring(1,(textlist[i].toString()).length) +'</p>';
+        }
+        if (textlist[i].substring(0,1)=="g") {
+            textlist[i]='<p style="color:#DDD605;display:inline;">'+ (textlist[i].toString()).substring(1,(textlist[i].toString()).length) +'</p>';
+        }
+    }
+    return (textlist.toString()).replaceAll(",","");
 }
